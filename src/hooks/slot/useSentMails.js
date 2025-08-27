f4b056ee79b9e8233a10670010858f1211c70bb2
Egortik71 <tikhonovegor2010@gmail.com>
2025-08-27 23:29:20 +0300

    feat(*): отправка писем

---
+++ src/hooks/slot/useSentMails.js
@@ -0,0 +1,56 @@
+import {useMutation, useQueryClient} from "react-query";
+
+import queryKeys from "../queryKeys";
+import {useCookies} from "react-cookie";
+import {message} from "antd";
+
+
+export function useSendMAils(): SendMail {
+    const queryClient = useQueryClient();
+    const [removeCookie] = useCookies()
+
+    let _callbackSuccess: any;
+    let _callbackError: any;
+
+    const _useApi = async (data: number[], afterSuccess: any, afterError: any) => {
+        _callbackSuccess = afterSuccess;
+        _callbackError = afterError;
+
+        const response = await fetch(`/api/slots/mails`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                // 'Authorization': `Bearer ${token}`,
+                'Accept': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+
+        if (response.status !== 200) {
+            return {error: `${response.status}: Письма не удалось отправить`}
+        }
+        return response.json()
+            .then((data) => data)
+    }
+
+    const {mutate} = useMutation({
+        mutationFn: ({data, afterSuccess, afterError}) => _useApi(data, afterSuccess, afterError),
+        onSuccess: (data: any) => {
+            if (data.message || data.error) {
+                message.error(data.message || data.error)
+                _callbackError && _callbackError(data);
+            } else {
+                queryClient.invalidateQueries([queryKeys.slots]);
+                message.success("Письма были отправлены");
+                _callbackSuccess && _callbackSuccess(data);
+            }
+        },
+        onError: (err: any) => {
+            _callbackError && _callbackError(err);
+        }
+
+    })
+    return mutate
+}
+
+type SendMail = ({data: number[], afterSuccess: any, afterError: any}) => void
---
+++ src/views/send-mail/SendMailPage.js
@@ -0,0 +1,113 @@
+import React, {memo, useMemo, useState} from "react";
+import {useNavigate} from "react-router";
+import {CContainer} from "@coreui/react";
+import { Button, Form, List, Select, Space, DatePicker } from 'antd';
+import dayjs from "dayjs";
+import {useSendMAils} from "../../hooks/slot/useSentMails";
+
+import { useClients } from "../../hooks/reference/client/useClients";
+
+const SendMailPage = memo(({activeTab}) => {
+    const navigate = useNavigate();
+    const [form] = Form.useForm();
+    const [clients, clientsStatus] = useClients();
+    const { RangePicker } = DatePicker;
+    const sendMails = useSendMAils();
+
+    const [generateResponse, setGenerateResponse] = useState(undefined);
+    const [generating, setGenerating] = useState(false);
+
+    const clientsOptions = useMemo(() => clients ? clients.map(s => ({ value: s.nClientId, label: s.vcName })) : [], [clients]);
+
+    const layout = {
+        labelCol: { span: 4 },
+        wrapperCol: { span: 20 },
+    };
+
+    const tailLayout = {
+        wrapperCol: { offset: 4, span: 20 },
+    };
+    
+    const onFinish = (values: any) => {
+        const {nClientIds} = values;
+        
+        // Очищаем ответы перед выполнением
+        setGenerateResponse(undefined);
+        setGenerating(true);
+
+        // Выполняем запроса
+        sendMails({
+            data: nClientIds,
+            afterSuccess: (data) => {
+                // Если успешно, то заполняем ответы для отображения
+                setGenerateResponse(data);
+                setGenerating(false);
+            },
+            afterError: (err) => {
+                // Снимаем режим выполнения
+                setGenerating(false);
+            }
+        })
+    };
+
+    const onReset = () => {
+        form.resetFields();
+    };
+
+    return (
+        <CContainer lg>
+            <h4 className={"mb-4 mt-2"}>Отправка писем</h4>
+
+            <CContainer className={"bg-white pt-3 ps-3 pe-3 pb-3 container"}>
+
+                <Form
+                    {...layout}
+                    form={form}
+                    name="control-hooks"
+                    onFinish={onFinish}
+                    initialValues={{ period: [dayjs(), dayjs().add(1, 'day')] }}
+                    >
+                    <Form.Item name="nClientIds" label="Клиенты" rules={[{ required: true }]}>
+                        <Select
+                            mode="multiple"
+                            allowClear
+                            style={{ width: '100%' }}
+                            placeholder="Нужно выбрать клиента"
+                            options={clientsOptions}
+                        />
+                    </Form.Item>
+
+                    
+
+                    <Form.Item {...tailLayout}>
+                        <Space>
+                        <Button type="primary" htmlType="submit" loading={generating}>
+                            Отправить
+                        </Button>
+                        <Button htmlType="button" onClick={onReset} loading={generating}>
+                            Сбросить
+                        </Button>
+                        </Space>
+                    </Form.Item>
+                </Form>
+
+                {
+                    generateResponse && (
+                        <List
+                            header={<div>Результат отправки писем</div>}
+                            bordered
+                            dataSource={generateResponse}
+                            renderItem={(item) => (
+                                <List.Item>
+                                    {item}
+                                </List.Item>
+                            )}
+                        />
+                    )
+                }
+            </CContainer>
+        </CContainer>
+    );
+})
+
+export default SendMailPage;
